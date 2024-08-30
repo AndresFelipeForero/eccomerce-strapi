6fea188eec7a3c09537f39e831e67e104e373649
Andres Forero <anfforerove@gmial.com>
2024-08-30 09:25:25 -0500

    lifecycle fixed

--- src/api/i-product/content-types/i-product/lifecycles.js
+++ src/api/i-product/content-types/i-product/lifecycles.js
@@ -1,13 +1,23 @@
 module.exports = {
-    beforeCreate(event) {
-      const { data } = event.params;
-  
-      data.finalPrice = data.price * (1 - data.discount/100);
-    },
-  
-    beforeUpdate(event) {
-      const { data } = event.params;
+  beforeCreate(event) {
+    const { data } = event.params;
 
-      data.finalPrice = data.price * (1 - data.discount/100);
-    },
+    // Validar que price y discount existen y son números válidos
+    if (typeof data.price === 'number' && typeof data.discount === 'number') {
+      data.finalPrice = data.price * (1 - data.discount / 100);
+    } else {
+      console.error('Error en beforeCreate: price o discount no son válidos');
+    }
+  },
+
+  beforeUpdate(event) {
+    const { data } = event.params;
+
+    // Validar que price y discount existen y son números válidos
+    if (typeof data.price === 'number' && typeof data.discount === 'number') {
+      data.finalPrice = data.price * (1 - data.discount / 100);
+    } else {
+      console.error('Error en beforeUpdate: price o discount no son válidos');
+    }
+  },
 };
